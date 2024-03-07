a2c73312d82a5129155977b6b9b38b1a5702ce15
kumarbro <kumarchavhanr@gmail.com>
2024-03-07 21:23:48 +0530

    127: Create save Post features Entity class added

---
+++ src/main/java/com/myblog/repositroy/PostRepository.java
@@ -0,0 +1,8 @@
+package com.myblog.repositroy;
+
+import com.myblog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long>{
+
+}
